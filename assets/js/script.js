88ff7987a5bbef061e9ff9c54606e018375fdaa7
FridaWikell <>
2023-12-20 09:42:19 +0000

    Add: Functions of randomise q&a + starter q&a

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ for (let i = 97; i <= 122; i++) {
     button.innerText = String.fromCharCode(i);
     keyboardAbc.appendChild(button);
 }
+
+// Randomise question
+function randomQuestion() {
+    let question = answersHint[Math.floor(Math.random() * answersHint.length)];
+    let currentAnswer = answersHint[0];
+    let currentHint = answersHint[1];
+}
+
+// Answers and hints
+let answersHint = [
+    {
+        answer: "super mario",
+        hint: "A hero with a green brother"
+    },
+    {
+        answer: "kid icarus"
+        hint: "His mission is to save goddess Palutena"
+    }
+];
