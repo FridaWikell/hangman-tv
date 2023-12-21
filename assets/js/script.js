3ac0fe7915f21c7370ab334ab2da938db2e1d71b
FridaWikell <>
2023-12-21 09:16:19 +0000

    Add: Functions to decide if input is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 // General
 let keyboardAbc = document.getElementById("keyboard");
 let answerDisplay = document.getElementById("correct-answer");
+let mistakes = 0;
+let usedLetters = [];
+let maximumMistakes = 6;
+
+maximumMistakes = document.getElementById("mistakes").innerText;
 
 
-// Creating keyboard - from https://www.codingnepalweb.com/build-hangman-game-html-javascript/
+// Creating keyboard - from https://github.com/simonjsuh/Vanilla-Javascript-Hangman-Game/blob/master/js/hangman.js
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="compareAnswers('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
 
-// HÄR SAKNAS EN EVENT LISTENER!
-for (let i = 97; i <= 122; i++) {
-    let button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardAbc.appendChild(button);
+    document.getElementById("keyboard").innerHTML = buttonsHTML;
 }
 
 
@@ -34,4 +46,48 @@ document.getElementById("hint").innerText = randomQuestionHint;
 answerDisplay.innerHTML = randomQuestionAnswer.split("").map(() => `<li class = "abcde"></li>`).join("");
 }
 
+// Compare input and question
+function compareAnswers(chosenLetter) {
+   if (randomQuestionAnswer.indexOf(chosenLetter) != 0) {
+        isGameWon();
+        hangmanWord();
+        disableLetter();
+   } else {
+        hangmanImage();
+        mistakes++
+        newMistakes();
+        disableLetter();
+        isGameOver();
+   }
+}
+
+function isGameWon()
+
+function hangmanWord()
+
+function hangmanImage() {
+    document.getElementById("hangman").src = "assets/images/hangman" + mistakes + ".png";
+}
+
+function newMistakes() {
+    document.getElementById("mistakes-made").innerHTML = mistakes;
+}
+
+function disableLetter() {
+    usedLetters.push(chosenLetter);
+    document.getElementById(chosenLetter).setAttribute("disabled");
+}
+
+function isGameOver() {
+    if (mistakes === maximumMistakes) {
+        document.getElementById("keybord").innerHTML = "You lost!";
+    }
+}
+
+function usedLetters () {
+
+}
+
 selectAQuestion();
+generateButtons();
+
